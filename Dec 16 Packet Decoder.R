ae582ecf47d80874cc156ce5ab283e4681d3d8d3
Ryan McCorvie <ryan@mccorvie.org>
2021-12-17 00:20:22 -0800

    put debugging code at end

--- Dec 16 Packet Decoder.R
+++ Dec 16 Packet Decoder.R
@@ -3,34 +3,6 @@ dir <- "~/Desktop/Advent-Code-2021/Dec 16"
 ff  <- "input"
 chunk <- readLines( file.path( dir, ff))
 
-# part1 test
-# chunk <- "8A004A801A8002F478"
-# chunk <- "620080001611562C8802118E34"
-# chunk <- "C0015000016115A2E0802F182340"
-# chunk <- "A0016C880162017C3686B18A3D4780"
-
-# part2 test
-
-#chunk <-"C200B40A82" 
-# chunk <-"04005AC33890" 
-# chunk <-"880086C3E88112" 
-# chunk <-"CE00C43D881120" 
-# chunk <-"D8005AC2A8F0" 
-# chunk <-"F600BC2D8F" 
-# chunk <-"9C005AC2F8F0" 
-# chunk <-"9C0141080250320F1802104A08"
-
-print_binary <- function( xx, width)
-{
-  for( shift in (width-1):0)
-  {
-    mask <- bitwShiftL( 1, shift )
-    cat( bitwShiftR( bitwAnd( xx,mask), shift))
-    xx <- bitwXor( mask, xx )
-  }
-}
-
-
 buffer <- buffer_sz <- 0L
 loc <- 1
 cum_bits_requested <- 0
@@ -92,11 +64,38 @@ packet_tree <- function()
     return( VV + sum(operator_args))
 
   funcs <- list( sum, prod, min, max, NULL, `>`, `<`, `==` )
-  func <- funcs[[ TT+1 ]]
-
-  return( as.numeric( do.call( func, as.list( operator_args ))))
+  return( as.numeric( do.call( funcs[[ TT+1 ]], as.list( operator_args ))))
 }
 
 part1<-F
 total <- packet_tree()
 sprintf( "%.100g",total)
+
+
+# part1 test
+# chunk <- "8A004A801A8002F478"
+# chunk <- "620080001611562C8802118E34"
+# chunk <- "C0015000016115A2E0802F182340"
+# chunk <- "A0016C880162017C3686B18A3D4780"
+
+# part2 test
+
+#chunk <-"C200B40A82" 
+# chunk <-"04005AC33890" 
+# chunk <-"880086C3E88112" 
+# chunk <-"CE00C43D881120" 
+# chunk <-"D8005AC2A8F0" 
+# chunk <-"F600BC2D8F" 
+# chunk <-"9C005AC2F8F0" 
+# chunk <-"9C0141080250320F1802104A08"
+
+print_binary <- function( xx, width)
+{
+  for( shift in (width-1):0)
+  {
+    mask <- bitwShiftL( 1, shift )
+    cat( bitwShiftR( bitwAnd( xx,mask), shift))
+    xx <- bitwXor( mask, xx )
+  }
+}
+
