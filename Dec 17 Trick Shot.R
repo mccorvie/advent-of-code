fb28d2f7ddb38efb5eea84c3d9a637599887f00a
Ryan McCorvie <ryan@mccorvie.org>
2021-12-17 00:20:40 -0800

    pretty straightforward simulation

---
+++ Dec 17 Trick Shot.R
@@ -0,0 +1,33 @@
+library( lubridate)
+target_x = c( 244,303 )
+target_y = c(-91, -54)
+
+# target_x= c(20, 30 )
+# target_y= c( -10,-5)
+
+tt_max <- 200
+tt <- 1:tt_max
+
+count <- 0
+maxy  <- 0
+for( vx0 in 1:303)
+  for( vy0 in -91:500)
+  {
+    xx <- ( 2*vx0 - tt+1 )*tt/2
+    if( vx0 < tt_max)
+      xx[(vx0+1):tt_max ] <- xx[vx0]
+
+    yy <- ( 2*vy0 - tt+1 )*tt/2
+    
+    if( any(target_x[1]<= xx & xx <= target_x[2] & target_y[1]<= yy & yy <= target_y[2]))
+    {
+      count <- count+1
+      maxy = max( max(yy), maxy)
+    }
+  }
+
+answer1 <- maxy
+answer2 <- count
+
+answer1
+answer2
