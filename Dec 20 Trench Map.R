f118c1e686c826898d9923c6463e869dc57fbb80
Ryan McCorvie <ryan@mccorvie.org>
2021-12-20 07:45:26 -0800

    boolean matrices, more generic handling of infinity pattern

--- Dec 20 Trench Map.R
+++ Dec 20 Trench Map.R
@@ -6,33 +6,31 @@ lines <- readLines( file.path( dir, ff))
 key <- str_split(lines[1], "", simplify = T)[1,]=="#"
 image <- str_split(lines[c(-1,-2)], "", simplify = T) == "#"
 
-
 pad <- 75
-padc <- matrix( 0, nrow = nrow(image), ncol = pad )
-padr <- matrix( 0, nrow = pad, ncol = ncol(image)+pad*2 )
-image <- rbind( padr, cbind( padc, image, padc ), padr)
+padc <- matrix( F, nrow = nrow(image), ncol = pad )
+padr <- matrix( F, nrow = pad, ncol = ncol(image)+pad*2 )
+new_image <- image <- rbind( padr, cbind( padc, image, padc ), padr)
 place_value <- 2^(8:0)
-
+infinity <- F
 for( i in 1:50)
 {
-  cat( i,"\n")
   for( rr in 2:(nrow(image)-1))
     for( cc in 2:(ncol(image)-1))
     {
       bits <- as.vector( t(image[(rr-1):(rr+1),(cc-1):(cc+1)]))
-      lookup <- sum( bits * place_value )+1
-      new_image[ rr, cc] <- key[lookup]    
+      new_image[ rr, cc] <- key[sum( bits * place_value )+1]    
     }
-  blink <- !new_image[1,1]
-  new_image[1,] <- new_image[nrow(new_image),]<-blink
-  new_image[,1] <- new_image[,ncol(new_image)]<-blink
+  # the borders get the infinity pattern
+  infinity <- key[ sum(place_value * infinity)+1]
+  new_image[1,] <- new_image[nrow(new_image),]<-infinity
+  new_image[,1] <- new_image[,ncol(new_image)]<-infinity
 
   image <- new_image
   
   ww1 <- which(apply(image, 1, any))
   ww2 <- which(apply(image, 2, any))
   
-  cat( "[", min(ww1), max(ww1), "]x[", min(ww2), max(ww2), "]\n")
+  cat( i, "size of image: [", min(ww1), max(ww1), "]x[", min(ww2), max(ww2), "]\n")
 }
 
 sum( image)
