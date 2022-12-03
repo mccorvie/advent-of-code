305c1b5bc092ff24c5ed00582aa3ba1f57c7c6f2
Ryan McCorvie <ryan@mccorvie.org>
2022-12-02 22:40:23 -0800

    simplify score calc

--- 03 - Rucksack Reorganization.R
+++ 03 - Rucksack Reorganization.R
@@ -2,22 +2,14 @@ library( tidyverse )
 dir <- "~/Desktop/aoc-input"
 ff  <- "input03"
 
-# input_raw <- read_delim( file.path( dir, ff) ) 
 input <- readLines( file.path( dir, ff))
-# input_raw <- scan(  file.path( dir, ff))
-
-lengths=  str_length(input)
-
+lengths =  str_length(input)
 ruck1 = str_sub( input, 1, lengths/2) |> str_split("")
 ruck2 = str_sub( input,lengths/2+1, lengths) |> str_split("")
 
-low <- letters
-upper <- c( rep( "", 26),LETTERS)
+all_lets <- c( letters,LETTERS)
 
-aa <- map2( ruck1, ruck2, intersect) |> map( ~ which(. == low)) |> map_dbl(~ ifelse( length(.)>0,.,0))
-bb <- map2( ruck1, ruck2, intersect) |> map( ~ which(. == upper)) |> map_dbl(~ ifelse( length(.)>0,.,0))
-
-part1 <- sum(aa+bb)
+part1 <- map2( ruck1, ruck2, intersect) |> map_dbl( ~ which(. == all_lets)) |> sum()
 
 gg <- split( input,rep( 1:(length(input)%/%3), each=3))
 badge <- NULL
@@ -25,10 +17,7 @@ for( idx in 1:length(gg))
 {
   rucks = str_split( gg[[idx]] , "")
   badge <- c( badge,intersect( intersect( rucks[[1]], rucks[[2]]), rucks[[3]]))
-  
 }
 
-aa <- badge |> map( ~ which(. == low)) |> map_dbl(~ ifelse( length(.)>0,.,0))
-bb <- badge |> map( ~ which(. == upper)) |> map_dbl(~ ifelse( length(.)>0,.,0))
+part2 <- badge |> map_dbl( ~ which(. == all_lets)) |> sum()
 
-part2 <- sum(aa+bb)
