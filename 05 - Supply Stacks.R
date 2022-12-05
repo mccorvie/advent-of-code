e7b77441bab345a6b8a4eee4a4755902ac5bed0f
Ryan McCorvie <ryan@mccorvie.org>
2022-12-04 22:31:05 -0800

    day 5, use adventr package in template,

---
+++ 05 - Supply Stacks.R
@@ -0,0 +1,39 @@
+
+library( tidyverse )
+library( adventr)
+#dir <- "~/Desktop/aoc-input"
+#ff  <- "input01"
+
+
+raw <- read_advent(day = 5, year = 2022 ) |> head(-1)
+
+stacks0 <- raw[1:8]
+stacks <- map( seq( from = 2, by  =4, length.out=9), ~ str_sub( stacks0, .,.) ) |>
+  map( ~ rev( .[.!= " "])) 
+stacks
+
+moves0 <- raw[11:(length( raw)-1)] |> str_split( " ", simplify = T)
+
+moves  <- cbind( strtoi( moves0[,2]),strtoi( moves0[,4]),strtoi( moves0[,6]))
+colnames( moves ) <- c( "crates", "from", "to")
+
+moves  <- tibble( raw = raw[11:length( raw)]) |>
+  extract( raw, c("ncrates", "from", "to"), "move (\\d+) from (\\d+) to (\\d+)", convert = T )
+
+
+aoc_part <- 1
+
+for( row in 1:nrow( moves ))
+{
+  move <- moves[row,]
+  
+  crate_list <- tail( stacks[[move$from]], move$ncrates )
+  stacks[[ move$from ]] <- head( stacks[[move$from]], -move$ncrates)
+  
+  if( aoc_part ==1 )
+    crate_list <- rev( crate_list )
+
+  stacks[[ move$to ]] <- c( stacks[[ move$to ]], crate_list )
+}
+
+stacks |> map(last) |>unlist() |> paste( collapse="")
--- TEMPLATE.R
+++ TEMPLATE.R
@@ -1,6 +1,15 @@
+#install.packages("devtools")
+#devtools::install_github("benjaminguinaudeau/adventr")
+if( file.exists("cookie.R"))
+  source( "cookie.R")
+
 library( tidyverse )
-dir <- "~/Desktop/aoc-input"
-ff  <- "input01"
+library( adventr)
+
+raw <- read_advent(day = xx, year=2022) 
+
+#dir <- "~/Desktop/aoc-input"
+#ff  <- "input01"
 
 # input_raw <- read_delim( file.path( dir, ff) ) 
 # input_raw <- readLines( file.path( dir, ff))
