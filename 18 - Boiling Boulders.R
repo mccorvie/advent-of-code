9d5955edf40a92ac36e5617b3f23c88e6870a970
Ryan McCorvie <ryan@mccorvie.org>
2022-12-19 11:24:20 -0800

    slight slicker version for 18

--- 18 - Boiling Boulders.R
+++ 18 - Boiling Boulders.R
@@ -8,8 +8,8 @@ library( adventr)
 library( unglue)
 
 day = 18
-raw  <- read_advent(day = day, year=2022) |> head(-1)
-
+#raw  <- read_advent(day = day, year=2022) |> head(-1)
+raw <- readLines( "input18")
 test <- c( 
   "1,1,1",
   "2,1,1"
@@ -37,17 +37,62 @@ input <- test
 coords<-tibble( input=input) |> 
   separate( input, c("x","y","z"), sep = ",", convert = T) |> 
   as.matrix()
-coords <- coords +1
-dd <- max( coords )
+dd <- max( coords ) + 1
+droplets <- array( F, dim=rep( dd+2,3))
+droplets[ coords+2 ] <- T
 
-space <- array( F, dim=rep( dd+1,3))
-space[coords]=T
+# all the coordinates, excluding either the leftmost slice or the rightmost slice
+shifty <- \(  dim ,  shift =  0 )
+{
+  cc <- rep( list( 1:(dd+2) ), 3 )
+  cc[[dim]] <- 1:(dd+1) + shift
+  do.call( expand.grid, cc ) |> as.matrix()
+}
 
-adj <- sum( space[1:dd,,] & space[ 2:(dd+1),,]) + 
-sum( space[,1:dd,] & space[ ,2:(dd+1),])+
-sum( space[,,1:dd] & space[ ,,2:(dd+1)])
+surface_area <- \(mm)
+{
+  adj <- \(dim) map(0:1, ~ mm[ shifty(dim, .)] ) |>  reduce( `&` ) |>  sum()
+  borders <- map_dbl( 1:3, adj ) |> sum()
+  sum(mm) * 6 - borders*2 
+}
 
-sum(space) * 6 - adj*2
+surface_area( droplets ) # part 1
+
+# fill the boundaries of the droplets with steam
+steam <- array( F, dim=rep( dd+2,3))
+steam[1,,]    = steam[,1,]    = steam[,,1] = T
+steam[dd+2,,] = steam[,dd+2,] = steam[,,dd+2] = T
+
+# Let the steam percolate
+nsteam = c( sum(steam),0 )
+while( nsteam[1] > nsteam[2])
+{
+  steam0 <- steam
+  for( dim in 1:3)
+  {
+    superpos <- map( 0:1, ~ steam0[shifty(dim,. )] ) |> reduce( `|` )
+    steam[shifty(dim,0)] <- superpos | steam[shifty(dim,0)]
+    steam[shifty(dim,1)] <- superpos | steam[shifty(dim,1)]
+  }
+  steam <- steam & !droplets  
+  nsteam <- c( sum( steam ), nsteam )
+  cat( nsteam[1], " ")
+}
+
+surface_area( droplets | !steam & !droplets ) # part 2
+
+##
+##. submitted solution
+##
+
+rep( list(1:2), 3)
+
+
+adj <- sum( droplets[1:dd,,] & droplets[ 2:(dd+1),,]) + 
+sum( droplets[,1:dd,] & droplets[ ,2:(dd+1),])+
+sum( droplets[,,1:dd] & droplets[ ,,2:(dd+1)])
+
+sum(droplets) * 6 - adj*2
 
 
 steam <- array( F, dim=rep( dd+1,3))
@@ -75,7 +120,7 @@ while( nsteam[1] > nsteam[2])
   
 
 
-  steam0 <- steam0 & !space  
+  steam0 <- steam0 & !droplets  
   
   steam <- steam0
   nsteam <- c( sum( steam ), nsteam )
@@ -83,12 +128,12 @@ while( nsteam[1] > nsteam[2])
 }
 
 steam
-space <- space | !steam & !space
+droplets <- droplets | !steam & !droplets
 
-adj <- sum( space[1:dd,,] & space[ 2:(dd+1),,]) + 
-  sum( space[,1:dd,] & space[ ,2:(dd+1),])+
-  sum( space[,,1:dd] & space[ ,,2:(dd+1)])
+adj <- sum( droplets[1:dd,,] & droplets[ 2:(dd+1),,]) + 
+  sum( droplets[,1:dd,] & droplets[ ,2:(dd+1),])+
+  sum( droplets[,,1:dd] & droplets[ ,,2:(dd+1)])
 
-sum( space )* 6 - adj*2
+sum( droplets )* 6 - adj*2
 
 
