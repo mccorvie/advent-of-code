8e4d154a923b2bde39fb4a7b2bb762993259356e
Ryan McCorvie <ryan@mccorvie.org>
2022-12-22 12:46:29 -0800

    checkpoint part 1

--- 22 - Monkey Map.R
+++ 22 - Monkey Map.R
@@ -18,87 +18,69 @@ test <- readLines( paste0( "test", day ))
 use_test = F
 input = if( use_test ) test else raw
 
-mmap0 <- str_split( input[1:(length(input)-3)], "")
+mmap0 <- str_split( input[1:(length(input)-2)], "")
 mmap <- map_dfr( 1:length( mmap0 ), \(row) tibble( row = row,  entry = mmap0[[row]] ) |> mutate( col = row_number() ) ) 
 mmap <- mmap |> filter( entry != " ")
-# cboundary <- mmap |> group_by( row ) |> summarize( cmin = min(col), cmax = max( col ))
-# rboundary <- mmap |> group_by( col ) |> summarize( rmin = min(row), rmax = max( row ))
-mmap
-mmap |> filter( row == 1) |> summarize( cmin = min(col)-1) |> pull( cmin)
 cmin <- mmap |> group_by( row ) |> summarize( cmin = min(col)-1 ) |> pull( cmin )
-cmin
 rmin <- mmap |> group_by( col ) |> summarize( rmin = min(row)-1 ) |> pull( rmin )
+rmin
 compass = c( ">", "v", "<", "^" )
 compass_val = 0:3
 names( compass_val ) = compass
 dir = c( "<" = -1, ">" = 1, "v"  =1, "^" = -1, "L" = -1, "R" = 1, "X" = 0 )
 
 idx_cycle = \( idx, len ) (idx-1) %% len +1
+idx_cycle( 6:56, 15)
 
+
+bb1 <- mmap |> group_by( row ) |> summarize( cmin = min(col), cmax = max( col )) 
+bb2 <- mmap |> group_by( col ) |> summarize( rmin = min(row), rmax = max( row) ) 
+
+input[199]
 currow = 1
-curcol = cmin[currow] + 1
+curcol = 1 + cmin[currow]
 curdir = ">"
+moves = str_extract_all( last( input ), "[:digit:]+")[[1]] |> as.numeric()
+turns = str_extract_all( last( input ), "[RL]")[[1]] |> c( "X" )
 curdir
 
-moves = str_extract_all( last( input ), "[:digit:]+")[[1]] |> as.numeric()
-turns  = str_extract_all( last( input ), "[RL]")[[1]] |> c( "X" )
 
 cat( "(", currow, ", ", curcol, ") ", curdir, "\n")
-
+last( moves)
+first( turns)
 for( movenum in 1:length( moves ))
 {
   nmove = moves[movenum]
-  nmove
-  
+
   cat( "move = ", nmove, "turn = ", turns[movenum])
   
   if( curdir == ">" || curdir == "<")
   {
     entries = mmap |> filter( row == currow ) |> pull( entry )
-    entries
     path = curcol - cmin[ currow ] + dir[ curdir ] * (1:nmove)
     path = idx_cycle( path, length( entries ) )
-    path
-    curcol
-    currow
-    
+
     stopmove = match(  "#", entries[path], nomatch = length(path)+1) -1
-    stopmove
-    nmove
-    path[stopmove]
-    path[1]
-    path
     if( stopmove > 0 )
       curcol = path[ stopmove ] + cmin[ currow ]
   } else {
     entries = mmap |> filter( col == curcol ) |> pull( entry )
     path = currow - rmin[ curcol ] + dir[ curdir ] * (1:nmove)
-    path
     path = idx_cycle( path, length( entries ) )
-    entries[path]
+    
     stopmove = match(  "#", entries[path], nomatch = length(path)+1) -1
-    path[stopmove]
-    stopmove
     if( stopmove > 0 )
       currow = path[ stopmove ] + rmin[ curcol ]
   }
   
   turn <- turns[movenum]
-  turn
-  curdir
   curdir <- compass[ idx_cycle( match( curdir, compass ) + dir[ turn ], 4 ) ]
   cat( " (", currow, ", ", curcol, ") ", curdir, "\n")
-  curcol
 }
-
+currow
+curcol
 1000 * currow + 4 * curcol + compass_val[curdir]
-
 curdir
-curdir = ">"
-dir[turn]
-match( ">", compass)
-turn = "R"
-compass[ match( curdir, compass) + dir[turn]]
 
-moves = str_extract_all( last( input ), "[:digit:]+")
-turns = str_extract_all( last( input ), "[RL]")
+compass_val
+# 125192
