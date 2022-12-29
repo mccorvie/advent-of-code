f9a69a59ede7670677bbab7d3fe3bfb054cbba24
Ryan McCorvie <ryan@mccorvie.org>
2022-12-29 10:56:26 -0800

    checkpoint progress

--- 19 - Not Enough Minerals.R
+++ 19 - Not Enough Minerals.R
@@ -14,7 +14,7 @@ test <- readLines( paste0( "test", day ))
 input<- raw
 input <- test
 
-prodbenefit = list( ore0 = c(1,0,0,0), ore = c(1,0,0,0),clay = c(0,1,0,0),obs = c(0,0,1,0),geo = c(0,0,0,1))
+prodbenefit = list( ore0 = c(1,0,0,0),ore = c(1,0,0,0),clay = c(0,1,0,0),obs = c(0,0,1,0),geo = c(0,0,0,1))
 buildcost <- list( 
   "ore0" = c(0,0,0,0),
   "ore" = c(-4,0,0,0),
@@ -39,38 +39,98 @@ print_build <- \( buildlist, tt )
 
 get_budget <- function( buildlist )
 {
-  buildtime <- map( names( buildcost ), \(robot) map_lgl( buildlist, \(day) robot %in% day ))
+  buildtime <- map( names( buildcost ), \(robot) map_dbl( buildlist, \(day) sum( robot == day )))
   names( buildtime ) = names( buildcost )
+  
+  buildtime
+  prodtime <- map( buildtime, \(x) cumsum( lag( x, default=0)))
   prodtime <- map( buildtime, \(x) lag( cumsum(x), default=0))
+  prodtime$ore = prodtime$ore+1
   costs    <- reduce( map2(  buildtime, buildcost, \( time, cost ) time %o% cost ), `+` )
+  costs
   benefits <- reduce( map2(  prodtime,  prodbenefit,  \( time, benefit ) time %o% benefit ), `+`)
-  apply( costs+benefits, 2, cumsum ) 
+  benefits
+  apply( benefits, 2, cumsum)
+  rbind( rep(0,4), apply( costs+benefits, 2, cumsum )  )
 }
+
 
 plan_build <- \( buildlist, tt )
 {
-  print_build( buildlist, tt )
+  if( runif(1)<1/1000 )
+    print_build( buildlist, tt )
   
+ # tt=7
   budget <- get_budget( buildlist )
-  best  <- max( budget[,4])
+  best   <- max( budget[,4])
   best
   budget
   for( nrobot in c( "geo", "obs", "clay", "ore"))
   {
-    bb <- budget[1:nrow( budget) >= tt,,drop=F]  |> sweep( 2, buildcost[[nrobot]],'+') 
-    dt <- match( T, apply( bb >= 0, 1, all), nomatch=NA)-1
+#    nrobot="clay"
+    
+    bb <- budget[1:nrow( budget) >= tt,,drop=F] |> sweep( 2, buildcost[[nrobot]],'+') 
+    tt
+    bb
+    dt <- match( T, apply( bb >= 0, 1, all), nomatch=NA)
     dt
     if( is.na( dt )) next
     nbuildlist <- buildlist
-    nbuildlist[[ tt + dt ]] = c( nbuildlist[[ tt + dt ]], nrobot )
+    cat( nrobot, tt, dt, tt + dt-1, "\n")
+    nbuildlist[[ tt + dt -1]] = c( nbuildlist[[ tt + dt -1]] , nrobot )
     nbuildlist
     best <- max( best, plan_build( nbuildlist, tt+dt ))
   }
   best
 }
+tt+dt-1
+nrobot
+tt+dt-1
+nbuildlist
+
+
 
 buildlist<-rep(list(NULL), 24)
 buildlist[[1]] <- c( "ore0")
+for( robot in c( "ore", "clay",  "obs", "geo" ))
+{
+  robot="ore"
+  tt=1
+  while( tt <= 24 )
+  {
+    print_build(buildlist, tt)
+    budget <- get_budget( buildlist )
+    bb <- budget[1:nrow( budget) >= tt,,drop=F]  |> sweep( 2, buildcost[[nrobot]],'+') 
+    dt <- match( T, apply( bb >= 0, 1, all), nomatch=NA)-1
+    if( is.na( dt ))
+      break
+    buildlist[[tt+dt]] = c( buildlist[[tt+dt]] , "ore")
+    tt <- tt+dt
+  }
+}
+
+
+budget
+buildlist
+
+buildlist
+bb <- budget[1:nrow( budget) > tt,,drop=F]  |> sweep( 2, buildcost[[nrobot]],'+') 
+
+
+bb <- budget[1:nrow( budget) > tt,,drop=F]  |> sweep( 2, buildcost[[nrobot]],'+') 
+dt <- match( T, apply( bb >= 0, 1, all), nomatch=NA)
+
+
+budget <- get_budget( buildlist )
+
+
+budget
+
+
+
+
+
+buildlist<-rep(list(NULL), 24)
 tt<-1
 plan_build( buildlist, 1 )
 
@@ -79,7 +139,11 @@ plan_build( buildlist, 1 )
 
 buildlist<-rep(list(NULL), 24)
 
-buildlist[[1]] <- c( "ore0")
+buildlist<-rep(list(NULL), 24)
+buildlist[[10]] <- c( "clay", "clay")
+get_budget( buildlist)
+
+#buildlist[[1]] <- c( "ore0")
 buildlist[[3]] <- c( "clay")
 buildlist[[17]] <- c( "obs")
 buildlist[[24]] <- c( "geo")
@@ -89,14 +153,13 @@ tt <- 24
 
 buildlist<-rep(list(NULL), 24)
 
-buildlist[[1]] <- c( "ore0")
 buildlist[[3]] <- c( "clay")
 buildlist[[5]] <- c( "clay")
 buildlist[[7]] <- c( "clay")
 buildlist[[11]] <- c( "obs")
 buildlist[[12]] <- c( "clay")
 buildlist[[15]] <- c( "obs")
-#buildlist[[18]] <- c( "geo")
-#buildlist[[21]] <- c( "geo")
+buildlist[[18]] <- c( "geo")
+buildlist[[21]] <- c( "geo")
 print_build( buildlist, 1 )
 
