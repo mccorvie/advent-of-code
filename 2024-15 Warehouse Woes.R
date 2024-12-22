ed4dfbe14291db9fc52bd0585d2c7cef12ad3fde
Ryan McCorvie <ryan@mccorvie.org>
2024-12-21 19:16:00 -0800

    ceckpoint part 2

--- 2024-15 Warehouse Woes.R
+++ 2024-15 Warehouse Woes.R
@@ -13,7 +13,7 @@ day <- 15
 raw <- read_advent(day = day, year=2024) |> head(-1)
 # raw  <- readLines( paste0( "input", day ))
 
-test <- readLines( paste0( "test", day, "a" ))
+test <- readLines( paste0( "test", day, "b" ))
 
 use_test = F
 input = if( use_test ) test else raw
@@ -66,29 +66,73 @@ print_warehouse( warehouse, pos )
 box_loc <- which( warehouse == "O", arr.ind=T) 
 sum(100*box_loc[,1] + box_loc[,2]-101)
 
+other_half_dpos <- list( "[" = c(0,1), "]"= c(0,-1))
+other_half      <- list( "[" = "]", "]" = "[")
 
-
-push_boxes_big <- \( warehouse, pos, dpos )
+push_boxes_big <- \( warehouse, pos, move, first=F)
 {
+  #first<-T
+  dpos   <- dpos_map[[move]]
+  coords <- map( 1:max(dim),\(x) pos+x*dpos) |> 
+    keep( \(v) all(all(v<=dim) & all(v >= c(1,1)))) |> 
+    reduce( rbind)
+  coords
+  path_to_edge <- warehouse[coords]
+  gap    <- which( path_to_edge == "." ) |> first()
+  if( is.na( gap )) return( NA )
+  if( gap == 1)     return( warehouse )
   
-  warehouse0 <- 666
+  box_stack <- path_to_edge[1:(gap-1)]
+  if( any( box_stack == "#" )) return( NA )
+
+  warehouse[ coords[1:gap,,drop=F]] <- c( ".", box_stack )
+  
+  if( move == "^" || move =="v")
+  {
+    first_box <- box_stack[1]
+    first_box
+    if( first )
+    {
+      warehouse <- push_boxes_big( warehouse, pos + other_half_dpos[[first_box]], move )
+      if( any( is.na(warehouse))) return( NA )
+    }
     
+    half_box <- which( box_stack == other_half[[first_box]] ) |> first()
+    half_box 
+    if( !is.na(half_box ))
+    {
+      push_pos  <- pos + other_half_dpos[[ other_half[[first_box]] ]] + (half_box-1)*dpos
+      warehouse <- push_boxes_big( warehouse, push_pos, move )
+      if( any( is.na(warehouse))) return( NA )
+    }
+  }
   
+  return( warehouse )
 }
 
 input_gap <- which( input == "")
 warehouse <- input[1:(input_gap-1)] 
-warehouse |> str_replace_all( "#","##") |> str_replace_all( "O", "[]")|> 
+warehouse <- warehouse |> str_replace_all( "#","##") |> str_replace_all( "O", "[]")|> 
   str_replace_all( "\\.", "\\.\\.") |> str_replace_all( "@", "@\\.") |> str_split( "", simplify = T)
-dim       <- dim(warehouse)[1]
+dim       <- dim(warehouse)
 pos       <- which( warehouse== "@", arr.ind=T) 
 warehouse[pos] <- "."
+moves     <- input[(input_gap+1):length(input)] |> str_split( "") |> unlist()
+
 
 for( move in moves )
 {
-  warehouse0 <- push_boxes_big(warehouse,pos,dpos)
+ # cat( "move ", move, "\n")
+#  print_warehouse( warehouse, pos )
+#  move <- moves[6]
+  
+  warehouse0 <- push_boxes_big(warehouse,pos,move,T)
   if( any(is.na( warehouse0 ))) next
   warehouse <- warehouse0
-  pos <- pos + dpos  
+  pos <- pos +  dpos_map[[move]] 
 }
+print_warehouse( warehouse, pos )
 
+
+box_loc <- which( warehouse == "[", arr.ind=T) 
+sum(100*box_loc[,1] + box_loc[,2]-101)
