da415a5f15049d5107570943eae69eca6c87bf1d
Ryan McCorvie <ryan@mccorvie.org>
2024-12-29 18:25:59 -0800

    another day done, 22 to be specific

---
+++ 2024-22 Monkey Market.R
@@ -0,0 +1,74 @@
+#install.packages("devtools")
+#devtools::install_github("benjaminguinaudeau/adventr")
+if( file.exists("cookie.R"))
+  source( "cookie.R")
+
+library( tidyverse )
+library( adventr)
+library( unglue)
+library( bitops )
+options(digits=20)
+
+day <- 22
+raw <- read_advent(day = day, year=2024) |> head(-1)
+# raw  <- readLines( paste0( "input", day ))
+
+test <- readLines( paste0( "test", day, "b" ))
+
+use_test = F
+input = if( use_test ) test else raw
+
+evolve <- \(n)
+{
+  #  n <- 9437184
+  n <- bitShiftL( n, 6 ) |> bitXor( n ) |> bitAnd( 16777215 )
+  n <- bitShiftR( n, 5 ) |> bitXor( n ) |> bitAnd( 16777215 )
+  n <- bitShiftL( n, 11 ) |> bitXor( n ) |> bitAnd( 16777215 )
+  n
+}
+
+
+evolve2000 <- \(n, last = T)
+{
+  cnt<<-cnt+1
+  if( cnt%%100 == 0 )
+    cat( cnt, "\n")
+  n_hist <- c( n )
+  for( k in 1:2000)
+    n_hist <- c( n_hist, evolve( last( n_hist )))
+  if( last ) return( last( n_hist ))
+  n_hist
+}
+
+#part 1
+input |> as.numeric() |> evolve2000() |> sum()
+
+key <- \(v) paste0( v, collapse = ",")
+
+make_price_table <- \(n)
+{
+  prices     <- evolve2000( n, F) %% 10 
+  len        <- length(prices)
+  price_diff <- diff( prices )
+  price_cache <- tibble( 
+    l4 = price_diff[1:(len-4)],
+    l3 = price_diff[2:(len-3)],
+    l2 = price_diff[3:(len-2)],
+    l1 = price_diff[4:(len-1)], 
+    price = prices[5:len] 
+  )
+  newname         <- "price"
+  names(newname ) <- paste0("price",n)
+  price_cache <- price_cache |> group_by( l1,l2,l3,l4) |> 
+    summarize( price = first(price), .groups = "drop") |> 
+    rename( all_of( newname ))
+  price_cache
+}
+
+tt<- input |> as.numeric() |> map( make_price_table ) 
+
+tt <- tt |> reduce( \(t1,t2) full_join( t1,t2, by= c("l1","l2","l3","l4"))) |> 
+  mutate( bananas = rowSums( across(starts_with("price")), na.rm=T))
+
+tt |> summarize(bananas = max(bananas))
+
