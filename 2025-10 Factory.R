be51fe237ff729d105428ae2074c8a0d0c7e8a3d
Ryan McCorvie <ryan@mccorvie.org>
2025-12-13 19:23:38 -0800

    it works on test

--- 2025-10 Factory.R
+++ 2025-10 Factory.R
@@ -6,7 +6,10 @@ if( file.exists("cookie.R"))
 library( tidyverse )
 library( adventr)
 library( unglue)
+
 library(glmnet)
+library(MASS)
+
 options(digits=20)
 
 day <- 10
@@ -15,7 +18,7 @@ raw <- read_advent(day = day, year=2025) |> head(-1)
 
 test <- readLines( sprintf("test%02da", day))
 
-use_test = T
+use_test = F
 input = if( use_test ) test else raw
 
 get_indicator_v <- \(line)
@@ -44,7 +47,7 @@ get_joltage_v <- \(line)
   str_extract( line, "\\[(.*)\\] (.*) \\{(.*)\\}", group=3) |>  str_split_1(",") |> as.numeric() 
 }
 
-presses_indicator <- \(line)
+n_press_indicator <- \(line)
 {
   
   indicator <- get_indicator_v( line ) 
@@ -65,7 +68,7 @@ presses_indicator <- \(line)
   NA 
 }
 
-map_dbl( input, presses_indicator) |> sum()
+map_dbl( input, n_press_indicator) |> sum()
 
 
 # stars and bars
@@ -75,101 +78,92 @@ compositions <- \( n, k )
   combn( n+k-1,k-1) |> apply( 2, \(x) diff( c( 0,x,n+k))-1) |> as.matrix()
 }
 
-
-presses_joltage0 <- \( buttons, joltage )
+recurse<<-0
+n_press_joltage0 <- \( buttons, joltage )
 {
+  recurse <<- recurse+1
+  if( recurse %% 1000 == 0)
+    cat( recurse, " buttons: ", ncol( buttons), " joltage: ", paste0(joltage, collapse=", "), "\n")
+
   bb<<-buttons
   jj <<-joltage
-   # buttons<<-bb
-   # joltage<<-jj
+   buttons<-bb
+   joltage<-jj
+  target_idx     <- which.min( joltage )
+  target_buttons <- which( buttons[target_idx,]==1 )
+  if( !length( target_buttons))
+    return( Inf)
+buttons
   
-  target_idx <- which.min(joltage)
   buttons
   joltage
   target_idx
-
-  target_buttons <- which( buttons[target_idx,]==1 )
   target_buttons
-  cat( nrow( buttons)," ", ncol( buttons), "\n")
-  
+
   comp <- compositions( joltage[ target_idx ], length( target_buttons ) )
   comp
 ncol(comp)
   pressed <- matrix( 0, ncol=ncol(comp), nrow = ncol(buttons))
   
   pressed[target_buttons, ] <- comp
+  pressed
+  buttons
   jremain = joltage - buttons %*% pressed
+  jremain
+  min_npress <- Inf
+  cc<-1
+  for( cc in 1:ncol(pressed))
+  {
+    live_j   <- abs(jremain[,cc]) > 1e-3
+    if( !any( live_j ))
+    {
+      cat( "--> exact\n")
+      return( joltage[target_idx])
+    }
+    live_j
+    
+    dead_b <- apply( buttons[live_j,,drop=F],2,sum) < 1e-3
+    if( any( dead_b))
+      next
+    
+    live_b <- apply( buttons[!live_j,,drop=F],2,sum) < 1e-3
+    if( !any( live_b))
+      next
+    
+    
+    
+    npress <- n_press_joltage0( buttons[ live_j, live_b, drop=F], jremain[live_j, cc ])
+    min_npress <- min( npress, min_npress )
+  }
   
-  if( any( apply( jremain, 2, \(x) sum( abs(x)))== 0))
-    return( joltage[target_idx])
-  
-  n_presses <- map_dbl( 1:ncol(pressed), \(c) presses_joltage0( buttons[-target_idx,-target_buttons], jremain[-target_idx, c ])) 
-
-  if( is.infinite( out ))
-    return( NA )
-  
-  return( n_presses + joltage[target_idx] )
+  return( min_npress + joltage[target_idx] )
 }
+
+n_press_joltage <- \( line )
+{
+  cat( "*")
+  joltage <- get_joltage_v( line )
+  buttons <- get_button_m( line, length(joltage))
+  n_press_joltage0( buttons, joltage )
+}
+bb
+jj
+line = input[1]
+line
+buttons
+
+map_dbl( input, n_press_joltage ) |> sum()
 
 
 bb
 jj
 
-buttons
-line = input[1]
-line
-joltage <- get_joltage_v( line )
-buttons <- get_button_m( line, length(joltage))
-
-presses_joltage0( buttons, joltage )
-
-joltage_remainder[-1,3]
-
-xx<-as.numeric( NA)
-xx
-mm <- min( xx, na.rm=T )
-is.infinite(mm)
-
-
-vv<-1:5
-vv
-vv[-c(3,4)]
-
-mm <- matrix(1:9,nrow=3)
-mm
-
-mm[-c(1,2),-3]
-
-which.min( 5:1)
-
-indicator <- get_indicator_v( line ) 
-buttons   <- get_button_m( line, length( indicator ) )
-nb = ncol( buttons )
-
-for( npress in 1:nb )
-{
-  pressed_idx_list  <- combn( nb, npress ) 
-  pressed <- matrix( 0, ncol=ncol( pressed_idx_list), nrow = nb )
-  for( c in 1:ncol( pressed_idx_list ))
-    pressed[pressed_idx_list[,c], c ] = 1
-  
-  # do the linear algebra and check for solutions
-  result <- apply( buttons %*% pressed %% 2 - indicator, 2, \(x) sum( abs(x)))
-  if( any( result < 1e-3 )) 
-    return( npress )
-}
-NA 
-
-
-
-
-
-
-line = input[1]
+## ridge regression
 
 joltage <- get_joltage_v( line )
 button  <- get_button_m (line, length( joltage))
 
+apply( button, 1, sum )
 
 fit <- glmnet( button, joltage, alpha=1)
 fit
@@ -177,21 +171,6 @@ best_lambda <- fit$lambda.min
 best_lambda
 coef(fit, s = "lambda.min")
 
-out$beta
-
-
-
-
-library(MASS)
 
 ridge_fit <- lm.ridge( joltage ~ button+0, lambda = 0.5)
-
 ridge_fit
-
-
-joltage
-button
-
-c( 1, 3,0,3,1,2)
-
-map( input, get_joltage_v )
